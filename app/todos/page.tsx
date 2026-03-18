ff6e385369f8d2a7dce52b6024851f9f846216be
fangkiigopramana <fangkiigo13@gmail.com>
2026-03-18 07:12:48 +0700

    supabase setup

---
+++ app/todos/page.tsx
@@ -0,0 +1,21 @@
+import { createClient } from '@/utils/supabase/server'
+import { cookies } from 'next/headers'
+
+export default async function Page() {
+  const cookieStore = await cookies()
+  const supabase = createClient(cookieStore)
+
+  const { data: todos } = await supabase.from('todos').select()
+  console.log(todos)
+
+  return (
+    <>
+    <h1>Hallo</h1>
+    <ul>
+      {todos?.map((todo) => (
+        <li key={todo.id}>{todo.name}</li>
+      ))}
+    </ul>
+    </>
+  )
+}
